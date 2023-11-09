544f56b394d7dcb0b59f9ceaf5c2c613dc8fee2e
MGutix <140160200+MGutix@users.noreply.github.com>
2023-11-09 22:47:24 +0900

    Pause

---
+++ script.js
@@ -0,0 +1,17 @@
+function mergeSort(arr){
+    //base case
+    if (arr.length <= 1){
+        return
+    }
+
+    //sort left half
+    // divide till min part (1 element)
+
+    
+    // merge in sorted
+    
+    
+}
+
+
+mergeSort([2,1])
